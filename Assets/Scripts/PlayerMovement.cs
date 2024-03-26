0e87a3f040b4808d6552e565c2838d2aaca63829
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2024-03-26 13:34:08 +0100

    Compliqué

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerInput _input;
+
+    InputAction _movement;
+
+    private float _speed = 10.0f;
+
+    private void Start()
+    {
+        _input = GetComponent<PlayerInput>();
+        _movement = _input.actions.FindAction("Move");
+    }
+    private void FixedUpdate()
+    {
+        OnMove();
+    }
+
+    void OnMove()
+    {
+        Vector2 direction = _movement.ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * _speed;
+    }
+}
---
+++ Assets/Scripts/PlayerPlant.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.Tracing;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerPlant : MonoBehaviour
+{
+    PlayerInput _input;
+
+    InputAction _plant;
+
+    private bool OnPlot = false;
+
+    public GameObject Plants;
+
+    private GameObject Plot;
+    private GameObject Plot2;
+
+    private void Start()
+    {
+        _input = GetComponent<PlayerInput>();
+        _plant = _input.actions.FindAction("Plant");
+    }
+
+    void OnPlant()
+    {
+        if (OnPlot == true)
+        {
+            Instantiate(Plants, Plot.transform);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Plot")
+        {
+            OnPlot = true;
+            Plot = collision.gameObject;
+            Debug.Log(Plot);
+        };
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Plot")
+        {
+            OnPlot = false;
+            Plot = Plot2;
+            Debug.Log(Plot);
+        }
+    }
+}
